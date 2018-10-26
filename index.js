d3918c40c362aa14d2cd13b41f4a7ecc988d10df
Justin Davis <justin_w_davis@yahoo.com>
2018-10-26 02:53:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() { return "Happy holidays!" }
 
 function happyHolidaysTo( name ) { return `Happy holidays, ${ name }!`}
 
-function happHolidayTo( holiday, name ) { return `Happy ${ holiday }, ${ name }`}
+function happyHolidayTo( holiday, name ) { return `Happy ${ holiday }, ${ name }`}
 
 function holidayCountdown( holiday, days ) { return `It's ${ days } days until ${ holiday }!`}
